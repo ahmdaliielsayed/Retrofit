a63df9322b3d9baa9c132433ecb02ff40da564eb
ahmedali500 <ahmedali666680@yahoo.com>
2021-06-21 03:47:28 +0200

    apply singleton with the best practice

--- app/src/main/java/com/example/retrofit/db/APIClient.java
+++ app/src/main/java/com/example/retrofit/db/APIClient.java
@@ -10,27 +10,31 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class APIClient {
 
-    private static Retrofit retrofit = null;
+    private static Retrofit retrofit;
 
     public static Retrofit getClient(){
+        if (retrofit == null) {
+            synchronized (APIClient.class) {
+                if (retrofit == null) {
+                    // optional steps but very important in debugging
+                    Gson gson = new GsonBuilder().serializeNulls().create();
 
-        // optional steps but very important in debugging
-        Gson gson = new GsonBuilder().serializeNulls().create();
+                    HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+                    loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
 
-        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
-        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+                    OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                            .addInterceptor(loggingInterceptor)
+                            .build();
+                    // optional steps but very important in debugging
 
-        OkHttpClient okHttpClient = new OkHttpClient.Builder()
-                .addInterceptor(loggingInterceptor)
-                .build();
-        // optional steps but very important in debugging
-
-        retrofit = new Retrofit.Builder()
-                .baseUrl("https://jsonplaceholder.typicode.com/")
-                .addConverterFactory(GsonConverterFactory.create(gson))
-                .client(okHttpClient)
-                .build();
-
+                    retrofit = new Retrofit.Builder()
+                            .baseUrl("https://jsonplaceholder.typicode.com/")
+                            .addConverterFactory(GsonConverterFactory.create(gson))
+                            .client(okHttpClient)
+                            .build();
+                }
+            }
+        }
         return retrofit;
     }
 }
